ed296159775311ea214bf9bdc4de1ec5e8333359
IlhamRayhandi <ilhamjunior22@gmail.com>
2020-11-01 21:18:51 +0700

    Materi 1

---
+++ sesi 1/hello.java
@@ -0,0 +1,12 @@
+public class hello{ //ini class
+
+	public static void main(String[] args){ //ini function
+		String nama = "Ilham";
+		byte umur = 16;
+		String no = "08235283984";
+
+System.out.println(nama); //ini logic
+System.out.println(umur);
+System.out.println(no);
+	}
+}
